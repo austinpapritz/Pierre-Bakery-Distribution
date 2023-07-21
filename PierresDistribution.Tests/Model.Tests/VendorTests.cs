bce8fbee70e831eb94e873474f325455b3cae17c
Austin Papritz <austin@papritz.dev>
2023-07-21 11:08:38 -0700

    vendor constructor test pass

--- PierresDistribution.Tests/Model.Tests/VendorTests.cs
+++ PierresDistribution.Tests/Model.Tests/VendorTests.cs
@@ -1,7 +1,17 @@
+using PierresDistribution.Models;
+
 namespace PierresDistribution.Tests;
 
     [TestClass]
     public class VendorTests 
     {
         // Tests go here
+        [TestMethod]
+        public void VendorConstructor_ConstructANewVendor_Vendor()
+        {
+            // Arrange
+            Vendor newVendor = new("Vendor Name");
+            // Assert
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
     }
--- PierresDistribution/Models/Vendor.cs
+++ PierresDistribution/Models/Vendor.cs
@@ -1,4 +1,10 @@
 namespace PierresDistribution.Models;
 public class Vendor
 {
+    public string Name { get; set; }
+
+    public Vendor(string name)
+    {
+        Name = name;
+    }
 }
