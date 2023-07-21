82b071c058f7fea23fb0ebfc3e4b9442bc2c00ad
Austin Papritz <austin@papritz.dev>
2023-07-21 11:22:58 -0700

    added a dispose to vendor test

--- PierresDistribution.Tests/Model.Tests/VendorTests.cs
+++ PierresDistribution.Tests/Model.Tests/VendorTests.cs
@@ -3,8 +3,14 @@ using PierresDistribution.Models;
 namespace PierresDistribution.Tests;
 
     [TestClass]
-    public class VendorTests 
+    public class VendorTests: IDisposable
     {
+
+    public void Dispose()
+    {
+        Vendor.ClearAll();
+    }
+    
         // Tests go here
         [TestMethod]
         public void VendorConstructor_ConstructANewVendor_Vendor()
@@ -13,5 +19,15 @@ namespace PierresDistribution.Tests;
             Vendor newVendor = new("Vendor Name");
             // Assert
             Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+
+        [TestMethod]
+        public void GetVendorById_FindVendorById_Vendor()
+        {
+            // Arrange
+            Vendor newVendor = new Vendor("Billy's Bakery");
+            // Act
+
+            // Assert
         }
     }
--- PierresDistribution/Models/Vendor.cs
+++ PierresDistribution/Models/Vendor.cs
@@ -1,10 +1,27 @@
 namespace PierresDistribution.Models;
 public class Vendor
 {
+    private static List<Vendor> _vendorList = new() { };
     public string Name { get; set; }
+    public int Id { get; }
+
+    public List<List<Order>> VendorOrders { get; set; }
 
     public Vendor(string name)
     {
         Name = name;
+        _vendorList.Add(this);
+        Id = _vendorList.Count;
+        VendorOrders = new List<List<Order>> { };
+    }
+
+    public static Vendor GetVendorById(int id)
+    {
+        // Code here
+    }
+
+    public static void ClearAll()
+    {
+        _vendorList.Clear();
     }
 }
